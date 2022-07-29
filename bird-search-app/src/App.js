eb5c2ba089c7cdab381a3de916cbb6ffe5b025ea
nmc1313 <98493277+nmc1313@users.noreply.github.com>
2022-07-29 17:41:51 -0400

    add files

---
+++ bird-search-app/src/App.js
@@ -0,0 +1,174 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// API KEY FOR BIRD WEBSITE mrjnc81so2mq
+//oriole search link https://media.ebird.org/catalog?taxonCode=oraori1
+
+const Pagination = ({ items, pageSize, onPageChange }) => {
+  const { Button } = ReactBootstrap;
+  if (items.length <= 1) return null;
+
+  let num = Math.ceil(items.length / pageSize);
+  let pages = range(1, num + 1);
+  const list = pages.map(page => {
+    return (
+      <Button key={page} onClick={onPageChange} className="page-item">
+        {page}
+      </Button>
+    );
+  });
+  return (
+    <nav>
+      <ul className="pagination">{list}</ul>
+    </nav>
+  );
+};
+const range = (start, end) => {
+  return Array(end - start + 1)
+    .fill(0)
+    .map((item, i) => start + i);
+};
+function paginate(items, pageNumber, pageSize) {
+  const start = (pageNumber - 1) * pageSize;
+  let page = items.slice(start, start + pageSize);
+  return page;
+}
+const useDataApi = (initialUrl, initialData) => {
+  const { useState, useEffect, useReducer } = React;
+  const [url, setUrl] = useState(initialUrl);
+
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    isLoading: false,
+    isError: false,
+    data: initialData
+  });
+
+  useEffect(() => {
+    let didCancel = false;
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_INIT" });
+      try {
+        const result = await axios(url);
+        if (!didCancel) {
+          dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+        }
+      } catch (error) {
+        if (!didCancel) {
+          dispatch({ type: "FETCH_FAILURE" });
+        }
+      }
+    };
+    fetchData();
+    return () => {
+      didCancel = true;
+    };
+  }, [url]);
+  return [state, setUrl];
+};
+const dataFetchReducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_INIT":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false
+      };
+    case "FETCH_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload
+      };
+    case "FETCH_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true
+      };
+    default:
+      throw new Error();
+  }
+};
+
+function App() {
+  const { Fragment, useState, useEffect, useReducer } = React;
+  const [query, setQuery] = useState("Orange Oriole - Icterus auratus");
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 10;
+  const [{ data, isLoading, isError }, doFetch] = useDataApi(
+    //"https://hn.algolia.com/api/v1/search?query=MIT",
+    "https://media.ebird.org/catalog?taxonCode=oraori1",
+    {
+      hits: []
+    }
+  );
+  const handlePageChange = e => {
+    setCurrentPage(Number(e.target.textContent));
+  };
+  let page = data.hits;
+  if (page.length >= 1) {
+    page = paginate(page, currentPage, pageSize);
+    console.log(`currentPage: ${currentPage}`);
+  }
+  return (
+    <Fragment>
+      <form
+        onSubmit={event => {
+          //doFetch("http://hn.algolia.com/api/v1/search?query=${query}");
+          doFetch("https://media.ebird.org/catalog?taxonCode={query}");
+          event.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      {isError && <div>Something went wrong ...</div>}
+
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <ul>
+          {page.map(item => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+      <Pagination
+        items={data.hits}
+        pageSize={pageSize}
+        onPageChange={handlePageChange}
+      ></Pagination>
+    </Fragment>
+  );
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
